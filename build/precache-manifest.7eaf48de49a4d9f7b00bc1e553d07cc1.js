cc8128f36d7e1db59e8f9e7459710e7b19f0bdc1
Bojan_S <bojanspasovski1@gmail.com>
2021-09-22 15:44:50 +0200

    initial commit

---
+++ build/precache-manifest.7eaf48de49a4d9f7b00bc1e553d07cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/static/css/main.5b937dfa.chunk.css"
+  },
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/static/js/main.2b56e97f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "dd1a920a6a6d474d9863af5c587518ea",
+    "url": "/index.html"
+  }
+];
